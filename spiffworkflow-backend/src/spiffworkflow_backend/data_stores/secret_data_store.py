29e37b41944c9398251b3a1a2d9f122f86009f4b
Jon Herron <jon.herron@yahoo.com>
2023-02-17 14:16:18 -0500

    Implement get/set

--- spiffworkflow-backend/src/spiffworkflow_backend/data_stores/secret_data_store.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/data_stores/secret_data_store.py
@@ -1,15 +1,22 @@
+from flask import g
+
 from SpiffWorkflow.bpmn.serializer.helpers.spec import BpmnSpecConverter
 from SpiffWorkflow.bpmn.specs.data_spec import BpmnDataStoreSpecification
 
 from spiffworkflow_backend.services.secret_service import SecretService
+from spiffworkflow_backend.services.user_service import UserService
 
 class SecretDataStore(BpmnDataStoreSpecification):
     """SecretDataStore - just for demonstration purposes."""
     def get(self, my_task):
-        raise NotImplementedError("SecretDataStore does not yet implement get.")
+        secret_value = SecretService.get_secret(self.name).value
+        my_task.data[self.name] = secret_value
 
     def set(self, my_task):
-        raise NotImplementedError("SecretDataStore does not yet implement set.")
+        secret_value = my_task.data[self.name]
+        user_id = g.user.id if UserService.has_user() else None
+        SecretService.update_secret(self.name, secret_value, user_id, True)
+        del my_task.data[self.name]
 
     def copy(self, source, destination, data_input=False, data_output=False):
         raise NotImplementedError("SecretDataStore does not yet implement copy.")
