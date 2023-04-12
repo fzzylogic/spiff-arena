200bb7190b33faf46e2a388df42a5e1c2357da53
Jon Herron <jon.herron@yahoo.com>
2023-04-11 21:41:19 -0400

    Getting run_pyl to pass

--- spiffworkflow-backend/lib/element-units/element_units/__init__.py
+++ spiffworkflow-backend/lib/element-units/element_units/__init__.py
@@ -1,2 +1,1 @@
-def this(is_a_test, really_long_line, will_it_format, i_dont_know, what_it_will_do, will_be_interesting, too_see_though):
-    pass
+"""Element Units."""
---
+++ spiffworkflow-backend/lib/element-units/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Element Units."""
